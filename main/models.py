3fb596fd789115d6257188231b5ff47bd5229ec3
Tony <ayeraldo.guillen@gmail.com>
2023-07-27 22:44:43 -0400

    FileField

--- main/models.py
+++ main/models.py
@@ -15,6 +15,7 @@ class Chat(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent')
     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received')
     content = models.TextField()
+    file = models.FileField(upload_to='chat_files/%Y/%m/%d/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
